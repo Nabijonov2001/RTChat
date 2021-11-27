5d3b52b9cbc759705f7f07393ceffe9bfcfa8184
Fazliddin <fazliddinnabijonov6@gmail.com>
2021-11-27 15:06:05 +0500

    express installed

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const http = require('http')
+const socket = require('socket.io')
 const path = require('path')
 
 const app  = express()
+const server = http.createServer(app)
+const io = socket(server)
 
 // Static folder
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 // Server port
-
 const port = process.env.PORT || 3000
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Server is working on port ${port}`)
 })
